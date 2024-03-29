f0bee9ffb448e34fe78004efdd5acee6674b747e
dizizmi <65617120+dizizmi@users.noreply.github.com>
2024-03-29 15:56:04 +0800

    retrieve user on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 
-import { NextPageContext } from 'next'
-import { getSession, signOut } from 'next-auth/react'
+import { NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 //protecting home routes
 export async function getServerSideProps(context: NextPageContext) { //client side
@@ -21,9 +23,12 @@ export async function getServerSideProps(context: NextPageContext) { //client si
 
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-4xl text-green-500"> Netflix Clone</h1>
+      <p className="text-white">Logged in as: { user?.email} </p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
     </>
   )
