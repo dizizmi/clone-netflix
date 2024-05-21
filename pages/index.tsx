ee2c70f3ad92aaddd2010a363faf3df397874a10
dizizmi <65617120+dizizmi@users.noreply.github.com>
2024-05-21 09:08:07 +0800

    removing user detail on homepage & navbar skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { getSession, signOut } from "next-auth/react"
 
 import useCurrentUser from "../hooks/useCurrentUser";
 
+import Navbar from "../components/Navbar"
+
 //protecting home routes
 export async function getServerSideProps(context: NextPageContext) { //client side
   const session = await getSession(context);
@@ -27,9 +29,7 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-4xl text-green-500"> Netflix Clone</h1>
-      <p className="text-white">Logged in as: { user?.email} </p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+      <Navbar />
     </>
   )
 }
