f02219f60058909c31f1b4c80478b6a327564f3f
dizizmi <65617120+dizizmi@users.noreply.github.com>
2024-05-21 09:29:27 +0800

    navbar header and skeleton for navbar items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,36 @@
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="w-full fixed z-40">
+            <div
+                className="
+                    px-4
+                    md:px-16
+                    py-6
+                    flex
+                    flex-row
+                    items-center
+                    transition
+                    duration-500
+                    bg-zinc-900
+                    bg-opacity-90
+                "
+            >
+                <div>
+                    <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
+                    <div 
+                        className="
+                            flex-row
+                            ml-8
+                            gap-7
+                            hidden
+                            lg-flex
+                        "
+                    >
+                        <NavbarItem />
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 
