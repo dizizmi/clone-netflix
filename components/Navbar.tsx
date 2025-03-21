1d8c31a3e00ef50ce98d880ba570ae42e69e1d67
dizizmi <65617120+dizizmi@users.noreply.github.com>
2025-03-21 15:12:36 +0800

    sml ss- drop down nav from 'browse' w click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,20 @@
 
+
+import { useState } from 'react';
 import { BsChevronDown } from 'react-icons/bs';
 
 import NavbarItem from './NavbarItem';
 import MobileMenu from './MobileMenu';
+import { useCallback } from 'react';
 
 const Navbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu((current) => !current);
+    }, []);
+
+    
     return (
         <nav className="w-full fixed z-40">
             <div
@@ -38,10 +48,10 @@ const Navbar = () => {
                     <NavbarItem label="My List"/>
                     <NavbarItem label="Browse by languages"/>
                 </div>
-                <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+                <div onClick= {toggleMobileMenu }className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
                     <p className="text-white text-sm">Browse</p>
                     <BsChevronDown className="text-white transition" />
-                    <MobileMenu visible />
+                    <MobileMenu visible= {showMobileMenu}/>
                 </div>
         </div>
     </nav>
